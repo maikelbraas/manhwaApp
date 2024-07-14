d0aba7193bb810410f92aa8501656c8ee2389a82
Maikel Braas <maikelbraas@gmail.com>
2024-07-14 08:55:47 +0200

    remove shift

--- utils/manhwaCheckReaper.js
+++ utils/manhwaCheckReaper.js
@@ -71,7 +71,7 @@ export default async function manhwaCheckReaper(req, res, next) {
                 chapterLinkSlice = jsonSingle.slice(jsonSingle.search('<div class="eplister" id="chapterlist">'), jsonSingle.search('var chapterSearchNotFound'));
                 let splitted = chapterLinkSlice.split('<li data-num="');
 
-                splitted.shift();
+                // splitted.shift();
                 for (let splits of splitted) {
                     try {
                         let link = splits.split('href=')[1].split('"')[1];
