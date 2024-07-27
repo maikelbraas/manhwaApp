0e61ffe15693577b90cc8be2cb79a18ce3ca79af
Maikel Braas <maikelbraas@gmail.com>
2024-07-27 08:49:16 +0200

    Update Manhwa.js

--- models/Manhwa.js
+++ models/Manhwa.js
@@ -62,7 +62,7 @@ class Manhwa {
     }
 
     static async getSavedManhwas(userid) {
-        const query = `SELECT manhwas.id, title, mid, slug, media, image, content, chapters, chapter, baseurl, manhwas.lastUpdate, status, reading FROM manhwas INNER JOIN chaptersSaved ON chaptersSaved.manhwaid = manhwas.mid WHERE userid = ? ORDER BY chaptersSaved.lastUpdate DESC, manhwas.lastUpdate DESC`;
+        const query = `SELECT manhwas.id, title, mid, slug, media, image, content, chapters, chapter, baseurl, manhwas.lastUpdate, status, reading FROM manhwas INNER JOIN chaptersSaved ON chaptersSaved.manhwaid = manhwas.mid WHERE userid = ? ORDER BY manhwas.lastUpdate DESC, chaptersSaved.lastUpdate DESC`;
         const [rows] = await connect.execute(query, [userid]);
         return rows;
     }
