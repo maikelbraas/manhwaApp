7ad9fd9674ae8b75f89f597464255368db3059ec
Maikel Braas <maikelbraas@gmail.com>
2024-08-04 19:44:54 +0200

    Update Manhwa.js

--- models/Manhwa.js
+++ models/Manhwa.js
@@ -93,10 +93,9 @@ class Manhwa {
 
 
     static async getAllManhwasLimit(page) {
-        page = (page * 6 - 6);
         try {
-            const query = `SELECT mid, title, content, slug, chapters, baseurl, lastUpdate, status, COUNT(id) OVER() as totalManhwas  FROM manhwas WHERE status != 'Dropped' ORDER BY title ASC LIMIT 6 OFFSET ?`;
-            const [rows] = await connect.execute(query, [page]);
+            const query = `SELECT mid, title, content, slug, chapters, baseurl, lastUpdate, status, COUNT(id) OVER() as totalManhwas  FROM manhwas WHERE status != 'Dropped' ORDER BY title ASC LIMIT 6 OFFSET ${(page * 6 - 6)}`;
+            const [rows] = await connect.execute(query);
             return rows;
         } catch (e) {
             console.log(e);
