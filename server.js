80c61cad6b09c59dd0e12258fd62933e2a8748f3
Maikel Braas <maikelbraas@gmail.com>
2024-08-12 08:44:34 +0200

    Update server.js

--- server.js
+++ server.js
@@ -41,7 +41,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(compression());
 app.use(express.static('public'))
 app.set('trust proxy', true);
-app.use(session({ secret: uuidv4(v4options).toString(), resave: true, saveUninitialized: true, cookie: { sameSite: 'lax', httpOnly: true, secure: true } }));
+app.use(session({ secret: uuidv4(v4options).toString(), resave: true, saveUninitialized: true, cookie: { sameSite: 'lax', httpOnly: true } }));
 
 app.use(expressVisitorCounter({ hook: counterId => counters[counterId] = (counters[counterId] || 0) + 1 }));
 app.use(flashMessage);
