ffb145cc02b610df742d9be43c9dd460bc9f827f
Maikel Braas <maikelbraas@gmail.com>
2024-08-19 10:14:35 +0200

    remove images

--- controllers/Manhwa.js
+++ controllers/Manhwa.js
@@ -17,8 +17,9 @@ class Manhwa {
             totalCreated += manhwasCreate.length;
             if (manhwasCreate.length > 0) {
                 for (let manhwa of manhwasCreate) {
-                    let image = await downloadImage(manhwa.mid, manhwa.image);
-                    await manhwaModel.create(manhwa.title, manhwa.mid, manhwa.slug, manhwa.description, manhwa.media, image, manhwa.chapters, manhwa.baseurl, manhwa.status);
+                    // let image = await downloadImage(manhwa.mid, manhwa.image);
+
+                    await manhwaModel.create(manhwa.title, manhwa.mid, manhwa.slug, manhwa.description, manhwa.media, manhwa.image, manhwa.chapters, manhwa.baseurl, manhwa.status);
                     await genreCheck(req, res, next, manhwa);
                 }
                 await this.resizeImages(manhwasCreate);
