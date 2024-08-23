654e1a2fc4a4b38db50351888af7d4702866e3a1
Maikel Braas <maikelbraas@gmail.com>
2024-08-23 13:30:03 +0200

    remove page

--- utils/checkAuth.js
+++ utils/checkAuth.js
@@ -4,5 +4,5 @@ export default function isAuthenticated(req, res, next) {
     if (req.isAuthenticated()) {
         return next();
     }
-    res.redirect('/login');
+    res.status(404).render('page_not_found.ejs', { title: '404: file not found', url: process.env.HOST_NAME + req.originalUrl });
 }
