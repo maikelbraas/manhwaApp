e591b76a37f883de48b249956fc1201811af6f64
Maikel Braas <maikelbraas@gmail.com>
2024-08-24 08:00:46 +0200

    redirect controle

--- utils/checkAuth.js
+++ utils/checkAuth.js
@@ -2,7 +2,12 @@
 
 export default function isAuthenticated(req, res, next) {
     if (req.isAuthenticated()) {
+        res.cookie('before', true, { sameSite: 'lax', secure: true });
         return next();
     }
-    res.status(404).render('page_not_found.ejs', { title: '404: file not found', url: process.env.HOST_NAME + req.originalUrl });
+    if (req.headers.cookie?.includes('before=true')) {
+        res.redirect('/aV46X3j9z9m6')
+    } else {
+        res.status(404).render('page_not_found.ejs', { title: '404: file not found', url: process.env.HOST_NAME + req.originalUrl });
+    }
 }
