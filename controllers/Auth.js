49626555132fa658ac6a23f160b9ab07f472944e
Maikel Braas <47853556+mbraas@users.noreply.github.com>
2025-04-02 13:10:17 +0200

    int to float

--- controllers/Auth.js
+++ controllers/Auth.js
@@ -83,7 +83,7 @@ class Auth {
             const manhwas = await manhwaModel.getSavedManhwas(userid);
             for (let manhwa of manhwas) {
                 const [link] = await manhwaModel.getCurrentChapter(manhwa.mid, parseFloat(manhwa.chapter).toFixed(1));
-                manhwa.link = manhwa.baseurl + '/title/' + manhwa.slug + "/chapter/" + parseInt(manhwa.chapter) + "/1";
+                manhwa.link = manhwa.baseurl + '/title/' + manhwa.slug + "/chapter/" + manhwa.chapter + "/1";
                 const next = await manhwaModel.getNextChapter(manhwa.mid, parseFloat(manhwa.chapter).toFixed(1));
                 manhwa.next = "";
                 if (next.length > 0)
