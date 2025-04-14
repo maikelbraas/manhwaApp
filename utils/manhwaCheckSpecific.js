26f2eee45d9850e19181dea098cc7e5739c37513
Maikel Braas <47853556+mbraas@users.noreply.github.com>
2025-04-14 10:57:21 +0200

    single demon update

--- utils/manhwaCheckSpecific.js
+++ utils/manhwaCheckSpecific.js
@@ -35,31 +35,32 @@ export default async function manhwaCheck(req, res, next) {
 
             let single = checkSingleManhwa;
 
-            let genreSlice = single.slice(single.search('class="categories"'), single.search('class="author"'));
-            genreSlice = genreSlice.replace(/<[^>]*>?/gm, '').split('\n');
-            let genres = [];
-            genreSlice.forEach(genre => genre.length > 2 ? genres.push(genre) : false);
-            genres.splice(0, 2)
-            let chapterSlice = single.slice(single.search('<strong class="chapter-title">'), single.search('<strong class="chapter-title">') + 80);
+            // let genreSlice = single.slice(single.search('class="categories"'), single.search('class="author"'));
+            // genreSlice = genreSlice.replace(/<[^>]*>?/gm, '').split('\n');
+            // let genres = [];
+            // genreSlice.forEach(genre => genre.length > 2 ? genres.push(genre) : false);
+            // genres.splice(0, 2)
+            let chapterSlice = single.slice(single.search('<div id="chapters-list">'), single.search('<div id="chapters-list">') + 80);
             let chapter = chapterSlice.replace(/[^0-9.]/g, '');
-            let statusSlice = single.slice(single.search('<small>Status</small>'), single.search('<small>Status</small>') + 80);
-            let status = statusSlice.replace(/<[^>]*>?/gm, '').split('\n')[1];
+            // let statusSlice = single.slice(single.search('<small>Status</small>'), single.search('<small>Status</small>') + 80);
+            // let status = statusSlice.replace(/<[^>]*>?/gm, '').split('\n')[1];
+            let status = "Ongoing";
             // let imageSlice = single.slice(single.search('src="https://readermc.org/images/thumbnails/'), single.search('src="https://readermc.org/images/thumbnails/') + 200);
             // let image = imageSlice.replace(/<[^>]*>?/gm, '').split('"')[1];
             let baseurl = "https://demonicscans.org/";
-            let descriptionSlice = single.slice(single.search('<p class="description">'), single.search('<section id="chapters" class="on">'));
-            let description = descriptionSlice.replace(/<[^>]*>?/gm, '').replace(/(\r\n|\n|\r)/gm, "");
+            // let descriptionSlice = single.slice(single.search('<p class="description">'), single.search('<section id="chapters" class="on">'));
+            // let description = descriptionSlice.replace(/<[^>]*>?/gm, '').replace(/(\r\n|\n|\r)/gm, "");
 
             let newObj = {
                 title: name,
                 mid: "mgdemon-" + mid,
-                description: description,
+                description: "",
                 slug: slug,
                 media: 404,
-                image: image,
+                image: "",
                 chapters: chapter,
                 baseurl: baseurl,
-                genres: genres,
+                genres: [""],
                 status: status,
                 manhwaChapters: []
             }
