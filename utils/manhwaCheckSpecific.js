202a0e69f9205ba46a4ec85cea18fbfa125948a9
Maikel Braas <47853556+mbraas@users.noreply.github.com>
2025-04-14 11:44:16 +0200

    Update manhwaCheckSpecific.js

--- utils/manhwaCheckSpecific.js
+++ utils/manhwaCheckSpecific.js
@@ -41,10 +41,8 @@ export default async function manhwaCheck(req, res, next) {
             // genreSlice.forEach(genre => genre.length > 2 ? genres.push(genre) : false);
             // genres.splice(0, 2)
             let chapterSlice = single.slice(single.search('class="chplinks"'), single.search('<span style="float:right;text-align: right;">'));
-            console.log(chapterSlice);
             chapterSlice = chapterSlice.split('Chapter ')[1];
             chapterSlice = chapterSlice.split('<span')[0];
-            console.log(chapterSlice);
             let chapter = chapterSlice.replace(/[^0-9.]/g, '');
             // let statusSlice = single.slice(single.search('<small>Status</small>'), single.search('<small>Status</small>') + 80);
             // let status = statusSlice.replace(/<[^>]*>?/gm, '').split('\n')[1];
