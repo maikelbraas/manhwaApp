4c29dc0b362d05fe81bb942a8841b14489487a62
Maikel Braas <47853556+mbraas@users.noreply.github.com>
2025-08-03 10:41:39 +0200

    Update Auth.js

--- controllers/Auth.js
+++ controllers/Auth.js
@@ -56,10 +56,8 @@ class Auth {
         for (let manhwa of manhwas) {
             if (manhwa.mid.includes('mgdemon')) {
                 // chapters = await checkSingleDemon(manhwa.mid);
-                if (content == null) {
-                    let responseSingle = await fetch(`https://demonicscans.org/manga/${manhwa.slug}`);
-                    content = await responseSingle.text();
-                }
+                let responseSingle = await fetch(`https://demonicscans.org/manga/${manhwa.slug}`);
+                content = await responseSingle.text();
                 //Get chapters links
                 content.slice(content.search('id="chapters-list"') - 200);
                 let piece = content.split('chapter=')[2];
