07ff65a797052867df894ef221227936304e6675
Maikel Braas <47853556+mbraas@users.noreply.github.com>
2025-08-03 10:01:30 +0200

    Update Auth.js

--- controllers/Auth.js
+++ controllers/Auth.js
@@ -54,7 +54,7 @@ class Auth {
         let maxChapter = 0;
         let manhwas = await manhwaModel.getSavedManhwas(userid);
         for (let manhwa of manhwas) {
-            if (manhwas[0].mid.includes('mgdemon')) {
+            if (manhwa.mid.includes('mgdemon')) {
                 // chapters = await checkSingleDemon(manhwa.mid);
 
                 if (content == null) {
