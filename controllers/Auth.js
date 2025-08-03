85677f0b8cb2fbbea903b7094c95b12559c2dca9
Maikel Braas <47853556+mbraas@users.noreply.github.com>
2025-08-03 10:32:46 +0200

    Update Auth.js

--- controllers/Auth.js
+++ controllers/Auth.js
@@ -64,8 +64,8 @@ class Auth {
                 //Get chapters links
                 content.slice(content.search('id="chapters-list"') - 100);
                 let piece = content.split('chapter=')[2];
-                console.log(piece);
                 maxChapter = piece.split('"')[0];
+                console.log(piece, maxChapter);
                 return;
             }
             // else {
